043bdc67addeb662551c42d342ba04359111c396
Hiroaki Tanaka <hiroaki-tanaka@issp.u-tokyo.ac.jp>
2022-01-20 10:11:51 +0900

    make Python and CPP directory

---
+++ Python/Config_example.py
@@ -0,0 +1,19 @@
+# Photoelectron_refraction
+# Config
+
+# For Gui
+fontFamilies=["Segoe UI", "Yu Gothic UI"]
+fontSize_normal=16
+
+ContentsMargins=[5,5,5,5]
+
+pen1=(0, 255, 0)
+pen2=(255, 0, 0)
+tickLength=-30
+
+# Physical constants
+Bohr_ang=0.529177
+Eh_eV=27.2114
+
+# configuration
+sigmaMax=5.0
